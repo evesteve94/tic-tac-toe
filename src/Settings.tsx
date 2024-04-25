b66c01730d789f5b11e5fb1b482fc94632223e94
Eva Björling <eva.bjorling@chasacademy.se>
2024-04-25 10:05:40 +0200

    settings improved

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -6,16 +6,18 @@ interface SettingsProps {
     size: number,
   }
 
-const Settings = ({winLength, setWinLength, gameStarted, size} : SettingsProps) => {
-  return (
-    <div className='settings'>
-    <button disabled={gameStarted} onClick={() => 
-      setWinLength(winLength !== 3 ? winLength - 1 : winLength)}>-</button>
-    <p><span style={{fontWeight: "bold"}}>{winLength}</span> in a row to win</p>
-    <button disabled={gameStarted} onClick={() => 
-      setWinLength(winLength !== size ? winLength + 1 : winLength)}>+</button>
-  </div>
-  )
-};
-
+  const Settings = ({winLength, setWinLength, gameStarted, size}: SettingsProps) => {
+    return (
+      <div className='settings'>
+        <button disabled={gameStarted} onClick={() => 
+          setWinLength(winLength !== 3 ? winLength - 1 : winLength)}>-</button>
+        <p style={{ color: gameStarted ? 'darkred' : 'inherit' }}>
+          <span style={{ fontWeight: "bold" }}>{winLength}</span> in a row to win
+          {gameStarted && <span style={{ marginLeft: '5px' }}>🔒</span>}
+        </p>
+        <button disabled={gameStarted} onClick={() => 
+          setWinLength(winLength !== size ? winLength + 1 : winLength)}>+</button>
+      </div>
+    );
+  };
 export default Settings
